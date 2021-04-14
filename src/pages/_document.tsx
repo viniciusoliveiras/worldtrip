35b3dca674d88971d602c4af9d3ba32c77b2ee22
viniciusoliveiras <vinitag190@gmail.com>
2021-04-14 20:34:53 -0300

    Adicionando favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,11 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap'
             rel='stylesheet'
           />
+          <link
+            rel='shortcut icon'
+            href='/images/favicon.png'
+            type='image/png'
+          />
         </Head>
         <body>
           <Main />
