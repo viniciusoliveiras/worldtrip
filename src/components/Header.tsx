bdcc745b6d13a12e302e7d43b9a0733f110ed2d9
viniciusoliveiras <vinitag190@gmail.com>
2021-04-14 16:45:11 -0300

    Componente header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,18 @@
+import { Flex, Image } from '@chakra-ui/react';
+
 export function Header() {
-  return <h1>Hi, there</h1>;
+  return (
+    <Flex
+      as='header'
+      w='100%'
+      maxWidth='1440'
+      h='24'
+      mx='auto'
+      px='6'
+      align='center'
+      justifyContent='center'
+    >
+      <Image src='../logo.svg' alt='WorldTrip' boxSize='auto' />
+    </Flex>
+  );
 }
