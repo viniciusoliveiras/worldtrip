94dc3d8a820a829b4609728b20508c052ec0ab22
viniciusoliveiras <vinitag190@gmail.com>
2021-04-14 20:38:34 -0300

    Adicionando titulo da pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
+
 import { Header } from '../components/Header';
 import { Banner } from '../components/Banner';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home - World Trip</title>
+      </Head>
       <Header />
       <Banner />
     </>
