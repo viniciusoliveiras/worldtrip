b336a6d3863ee637b1476d596563f897f973b5ac
viniciusoliveiras <vinitag190@gmail.com>
2021-04-21 23:39:43 -0300

    Continents mobile

--- src/components/Continents.tsx
+++ src/components/Continents.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, useBreakpointValue } from '@chakra-ui/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -14,6 +14,11 @@ interface ContinentsProps {
 }
 
 export function Continents() {
+  const isWideVersion = useBreakpointValue({
+    sm: false,
+    md: true,
+  });
+
   const [continents, setContinents] = useState<ContinentsProps[]>([]);
 
   useEffect(() => {
@@ -24,58 +29,113 @@ export function Continents() {
 
   console.log(continents);
   return (
-    <Flex
-      mx='auto'
-      mt='14'
-      mb='10'
-      maxWidth={{ md: '700px', lg: '900px', xl: '1240px' }}
-    >
-      <Swiper
-        spaceBetween={0}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        {continents.map((continent) => (
-          <SwiperSlide key={continent.id}>
-            <Flex
-              width='100%'
-              height={[178, 238, 450]}
-              bgImage={`url('${continent.image}')`}
-              bgRepeat='no-repeat'
-              bgPosition='100% 30%'
-              bgSize='cover'
-              justifyContent='center'
-              alignItems='center'
-              flexDirection='column'
-              style={{ boxShadow: 'inset 0 0 0 50vw rgba(0,0,0,0.5)' }}
-            >
-              <Text
-                fontWeight='bold'
-                fontSize={{ md: '3xl', lg: '4xl', xl: '5xl' }}
-                color='gray.50'
-                textAlign='center'
-                align='center'
-              >
-                {continent.name}
-              </Text>
-              <Text
-                fontWeight='bold'
-                fontSize={{ md: 'lg', lg: 'xl', xl: '2xl' }}
-                color='gray.50'
-                textAlign='center'
-                align='center'
-                mt='4'
-              >
-                {continent.description}
-              </Text>
-            </Flex>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </Flex>
+    <>
+      {isWideVersion && (
+        <Flex
+          mx='auto'
+          mt='14'
+          mb='10'
+          maxWidth={{ md: '700px', lg: '900px', xl: '1240px' }}
+        >
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            {continents.map((continent) => (
+              <SwiperSlide key={continent.id}>
+                <Flex
+                  width='100%'
+                  height={[178, 238, 450]}
+                  bgImage={`url('${continent.image}')`}
+                  bgRepeat='no-repeat'
+                  bgPosition='100% 30%'
+                  bgSize='cover'
+                  justifyContent='center'
+                  alignItems='center'
+                  flexDirection='column'
+                  style={{ boxShadow: 'inset 0 0 0 50vw rgba(0,0,0,0.5)' }}
+                >
+                  <Text
+                    fontWeight='bold'
+                    fontSize={{ md: '3xl', lg: '4xl', xl: '5xl' }}
+                    color='gray.50'
+                    textAlign='center'
+                    align='center'
+                  >
+                    {continent.name}
+                  </Text>
+                  <Text
+                    fontWeight='bold'
+                    fontSize={{ md: 'lg', lg: 'xl', xl: '2xl' }}
+                    color='gray.50'
+                    textAlign='center'
+                    align='center'
+                    mt='4'
+                  >
+                    {continent.description}
+                  </Text>
+                </Flex>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Flex>
+      )}
+
+      {!isWideVersion && (
+        <Flex mx='auto' mt='5' mb='6' w='100%' h='60'>
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            {continents.map((continent) => (
+              <SwiperSlide key={continent.id}>
+                <Flex
+                  width='100%'
+                  height='100%'
+                  bgImage={`url('${continent.image}')`}
+                  bgRepeat='no-repeat'
+                  bgPosition='100% 30%'
+                  bgSize='cover'
+                  justifyContent='center'
+                  alignItems='center'
+                  flexDirection='column'
+                  style={{ boxShadow: 'inset 0 0 0 50vw rgba(0,0,0,0.5)' }}
+                >
+                  <Text
+                    fontWeight='bold'
+                    fontSize={{ md: '3xl', lg: '4xl', xl: '5xl' }}
+                    color='gray.50'
+                    textAlign='center'
+                    align='center'
+                  >
+                    {continent.name}
+                  </Text>
+                  <Text
+                    fontWeight='bold'
+                    fontSize={{ md: 'lg', lg: 'xl', xl: '2xl' }}
+                    color='gray.50'
+                    textAlign='center'
+                    align='center'
+                    mt='4'
+                  >
+                    {continent.description}
+                  </Text>
+                </Flex>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Flex>
+      )}
+    </>
   );
 }
